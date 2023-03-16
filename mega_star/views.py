de058fb688c4c4ea2e940e3e52534f33581db83f
Rajeshgoriparthi <goriparthiraj77@gmail.com>
2023-03-16 21:48:10 +0530

    first commit

---
+++ mega_star/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def chiru(request):
+    return render(request,'chiru.html')
+
+
+def charan(request):
+    return render(request,'charan.html')
